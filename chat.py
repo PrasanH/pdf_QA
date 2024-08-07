380c0fd42ab5699a22ca3be65d0c44c7f0430dae
PrasanH <prasanna99h@gmail.com>
2024-08-07 11:04:27 +0200

    removed pycache and updated chat.py

--- chat.py
+++ chat.py
@@ -2,10 +2,12 @@ import os
 import openai
 from dotenv import load_dotenv
 import streamlit as st
+from openai import OpenAI
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv(r'.\project\.env')
 
+my_api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = my_api_key)
 
 messages = [
     {
@@ -14,8 +16,9 @@ messages = [
     },
 ]
 
-models = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"]
-model = models[0]
+models = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o", "gpt-4o-mini"]
+model = models[3]
+print('Interacting with', model)
 
 while True:
     message = input("User : ")
@@ -23,7 +26,7 @@ while True:
         messages.append(
             {"role": "user", "content": message},
         )
-        chat = openai.ChatCompletion.create(model=model, messages=messages)
+        chat = client.chat.completions.create(model=model, messages=messages)
 
     reply = chat.choices[0].message.content
     print(f"ChatGPT: {reply}")
